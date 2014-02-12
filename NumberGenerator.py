546696b3af008a7b5cda1d5397eb98d3da3c5ca5
31downto0 <dgdalessandro92@gmail.com>
2014-02-12 11:17:48 -0600

    made NumberGenerator.py easier to use

--- NumberGenerator.py
+++ NumberGenerator.py
@@ -8,13 +8,17 @@ outputFile2 = "RandomNumbers.txt"
 fo = open(outputFile1, "w")
 of = open(outputFile2, "w")
 nums = [0]*256
+noiseMean = 60
+noiseStdDev = 30
+dataMean = 180
+dataStdDev = 10
 
-for i in xrange(0,100000):
+for i in xrange(0,10000000):
 	threshold = random.random()
 	if threshold <= .05:
-		randomnumber = int(random.gauss(180,10))
+		randomnumber = int(random.gauss(dataMean,dataStdDev))
 	else:
-		randomnumber = int(random.gauss(60,30))
+		randomnumber = int(random.gauss(noiseMean,noiseStdDev))
 	if randomnumber <= 255 and randomnumber >= 0:
 		nums[randomnumber] += 1
 		of.write(str(randomnumber) + '\n')
