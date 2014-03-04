510e5bceebb8a5cdc8958c510abc9c284c5c8352
Andrew Deck <adeck@shell.cec.wustl.edu>
2014-03-04 02:54:41 -0600

    Replaced inline kernels with external kernels in test modules.

--- veritas/src/test/scala/FirstHalfTest.scala
+++ veritas/src/test/scala/FirstHalfTest.scala
@@ -1,3 +1,5 @@
+import veritas.kernels.{Mean, StdDev, Threshold}
+
 import scalapipe.kernels._
 import scalapipe.dsl._
 
@@ -47,143 +49,9 @@ object FirstHalfTest extends App {
 			}
 		}
 
-		val Mean = new Kernel("Mean") {
-			val pixelData = input(UNSIGNED32)
-			val mean0 = output(FLOAT32)
-			val mean1 = output(FLOAT32)
-			val mean2 = output(FLOAT32)
-
-			val outputCount = config(UNSIGNED32, 'outputCount, 1000)
-
-			val count = local(UNSIGNED32, 0)
-			val state = local(UNSIGNED32, 0)
-			val means = local(Vector(FLOAT32, pixels))
-			val temp = local(FLOAT32, 0)
-			val tempPixel = local(UNSIGNED32, 0)
-
-			val i = local(UNSIGNED32, 0)
-
-			switch(state) {
-
-				when(0) {
-					count = 0
-					i = 0
-					while(i < pixels) {
-						means(i) = 0
-						i += 1
-					}
-					state = 1
-				}
-
-				when(1) {
-					if(count <= outputCount) {
-						i = 0
-						while(i < pixels) {
-							tempPixel = cast(pixelData, FLOAT32)
-							if(i == 0) {
-								//stdio.printf("Pixel 0:\t%.3f\tIncoming Pixel:\t%.3f\n", means(i), tempPixel)
-							}
-							temp = means(i) * count
-							temp += tempPixel
-							means(i) = temp / (count + 1)
-							i += 1
-						}
-						count += 1
-					} else {
-						i = 0
-						while(i < pixels) {
-							mean0 = means(i)
-							mean1 = means(i)
-							mean2 = means(i)
-							i += 1
-						}
-						state = 0
-					}
-				}
-			}
-		}
-
-		val StdDev = new Kernel("StdDev") {
-			val mean = input(FLOAT32)
-			val pixelData = input(UNSIGNED32)
-			val stdDev0 = output(FLOAT32)
-			val stdDev1 = output(FLOAT32)
-
-			val outputCount = config(UNSIGNED32, 'outputCount, nums)
-
-			val count = local(UNSIGNED32, 0)
-			val means = local(Vector(FLOAT32, pixels))
-			val stdDevs0 = local(Vector(FLOAT32, pixels))
-			val stdDevs1 = local(Vector(FLOAT32, pixels))
-			val temp = local(FLOAT32, 0)
-
-			val state = local(UNSIGNED8, 0)
-
-			val i = local(UNSIGNED32, 0)
-
-			switch(state) {
-
-				when(0) {
-					count = 1
-					i = 0
-					while(i < pixels) {
-						stdDevs0(i) = 0
-						stdDevs1(i) = 0
-						i += 1
-					}
-					state = 1
-				}
-
-				when(1) {
-					if(count <= outputCount) {
-						i = 0
-						while(i < pixels) {
-							temp = cast(pixelData, FLOAT32)
-							stdDevs0(i) += temp * temp
-							stdDevs1(i) += temp
-							i += 1
-						}
-						count += 1
-					} else {
-						i = 0
-						while(i < pixels) {
-							means(i) = mean
-							i += 1
-						}
-
-						i = 0
-						while(i < pixels) {
-							stdDevs0(i) -= (2 * means(i) * stdDevs1(i))
-							stdDevs0(i) += outputCount * means(i) * means(i)
-							stdDevs0(i) = stdDevs0(i) / (outputCount - 1)
-							stdDevs0(i) = sqrt(stdDevs0(i))
-							stdDev0 = stdDevs0(i)
-							stdDev1 = stdDevs0(i)
-							i += 1
-						}
-						state = 0
-					}
-				}
-
-			}
-		}
-
-		val Threshold = new Kernel("Threshold") {
-			val mean = input(FLOAT32)
-			val stdDev = input(FLOAT32)
-
-			val loThreshold = output(UNSIGNED32)
-			val hiThreshold = output(UNSIGNED32)
-
-			val tempMean = local(FLOAT32)
-			val tempStdDev = local(FLOAT32)
-
-			tempMean = mean
-			tempStdDev = stdDev
-
-			loThreshold = cast((tempMean + (1.5 * tempStdDev)), UNSIGNED32)
-			hiThreshold = cast((tempMean + (2 * tempStdDev)), UNSIGNED32)
-		}
+		val Mean0 = new Mean("Mean")
+		val StdDev0 = new StdDev("StdDev")
+		val Threshold0 = new Threshold("Threshold")
 
 		val Print = new Kernel("Print") {
 			val mean = input(FLOAT32)
@@ -216,9 +84,9 @@ object FirstHalfTest extends App {
 			//val newWidth = 7
 			//val newHeight = 7
 			val random = RandomReader()
-			val mean = Mean(random(0))
-			val stdDev = StdDev(mean(0), random(1))
-			val threshold = Threshold(mean(1), stdDev(0))
+			val mean = Mean0(random(0))
+			val stdDev = StdDev0(mean(0), random(1))
+			val threshold = Threshold0(mean(1), stdDev(0))
 			Print(mean(2), stdDev(1), threshold(0), threshold(1))
 		}
 
--- veritas/src/test/scala/RLETest.scala
+++ veritas/src/test/scala/RLETest.scala
@@ -1,3 +1,5 @@
+import veritas.kernels.RunLengthEncode
+
 import scalapipe.kernels._
 import scalapipe.dsl._
 
@@ -33,57 +35,11 @@ object RLE_test extends App {
 		stdio.printf(" %u \n", x0)
 	}	
 
-	val RLE = new Kernel("RLE") {
-	
-		val pixelData = input(UNSIGNED32)
-		val encoded_data = output(UNSIGNED32)
-
-		// Don't run encode across frames
-		//val pixelCount = config(UNSIGNED16, 'pixelCount, 50)
-		val pixelCount = local(UNSIGNED16, 50)
-		val count = local(UNSIGNED16, 0)
-		val runLength = local(UNSIGNED16, 0)
-		val prevZero = local(UNSIGNED8, 0) //is there a bool type? 
-		val pixel = local(UNSIGNED32,0) // want to set this local variable to pixel input 
-		pixel = pixelData
-
-		if(pixel ==0){
-			if(prevZero == 0){
-				prevZero = 1
-				runLength+=1
-				encoded_data =0
-
-			}
-			else{
-				runLength+=1
-			}
-		}
-		else {
-			if(prevZero == 0){
-				encoded_data = pixel	
-			}
-			else{
-				encoded_data = runLength
-				prevZero = 0
-				runLength = 0
-				encoded_data = pixel	
-			}	
-		}
-		count+=1;
-		if(count == pixelCount){
-			count =0
-			if(prevZero ==1){
-				encoded_data = runLength	
-				runLength = 0	
-			}	
-			prevZero = 0	
-		}
-	
-	}
+	val UUT = new RunLengthEncode("RLE")
 	
 	val app = new Application {
 		val random = Random()
-		val encoded = RLE(random)
+		val encoded = UUT(random)
 		Print(encoded)
 	}
 
