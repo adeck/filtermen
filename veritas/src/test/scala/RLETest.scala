b8ec2be65e5c0dacb34e3fe6d898b45e742fedb8
sberul <sberul@gmail.com>
2014-03-18 12:26:01 -0500

    Update RLETest.scala
    Changed to UNSIGNED32

--- veritas/src/test/scala/RLETest.scala
+++ veritas/src/test/scala/RLETest.scala
@@ -7,11 +7,11 @@ object RLE_test extends App {
 
 	val Random = new Kernel("Random") {
 		//val iter = input(UNSIGNED32)
-		val iter = local(UNSIGNED32, 50)	
-		val y0 = output (UNSIGNED32)
+		val iter = local(UNSIGNED16, 50)	
+		val y0 = output (UNSIGNED16)
 
-		val temp = local(UNSIGNED32,0)
-		val i = local(UNSIGNED32, 0)
+		val temp = local(UNSIGNED16,0)
+		val i = local(UNSIGNED16, 0)
 
 		temp = stdio.rand() %100
 		if(temp < 40) {
@@ -31,7 +31,7 @@ object RLE_test extends App {
 	}
 	
 	val Print = new Kernel("Print") {
-		val x0 = input(UNSIGNED32)
+		val x0 = input(UNSIGNED16)
 		stdio.printf(" %u \n", x0)
 	}	
 
