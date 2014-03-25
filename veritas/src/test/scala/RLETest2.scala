51a9fea0f16cae3ba07fc4a5136f4cf4dedb6df3
sberul <sberul@gmail.com>
2014-03-25 14:22:34 -0500

    Update RLETest2.scala

--- veritas/src/test/scala/RLETest2.scala
+++ veritas/src/test/scala/RLETest2.scala
@@ -30,6 +30,7 @@ object RLE_test2 extends App {
 	val Print = new Kernel("Print") {
 		val x0 = input(UNSIGNED16)
 		stdio.printf(" %u \n", x0)
+		stdio.exit(0)
 	}	
 
 	val UUT = new RunLengthEncode("RLE")
@@ -38,8 +39,8 @@ object RLE_test2 extends App {
 		val num = NumSequence()
 		val encoded = UUT(num)
 		Print(encoded)
-		map(ANY_KERNEL -> RunLengthEncode, CPU2FPGA())
-		map(RunLengthEncode -> ANY_KERNEL, FPGA2CPU())
+		map(ANY_KERNEL -> UUT, CPU2FPGA())
+		map(UUT -> ANY_KERNEL, FPGA2CPU())
 	}
 
 	app.emit("RLE_test2")
