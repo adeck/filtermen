8a63c3081ce36ca4450a4bd3f1f51223e5a6d7d6
sberul <sberul@gmail.com>
2014-04-17 15:33:59 -0500

    Update DoLP.scala

--- polarization/src/main/scala/kernels/DoLP.scala
+++ polarization/src/main/scala/kernels/DoLP.scala
@@ -5,13 +5,12 @@ import scalapipe.dsl._
 class DoLP(_name:String) extends Kernel(_name:String)
 {
 	val parms = input(Vector(FLOAT32, 4))
-	val s0 = local(FLOAT32, parms(0))
-	val s1 = local(FLOAT32, parms(1))
-	val s2 = local(FLOAT32, parms(2))
-	
-
 	val dataOut = output(FLOAT32)
+	val s1 = local(FLOAT32, 0)
+	val s2 = local(FLOAT32, 0)
+	s1 = parms(1)
+	s2 = parms(2)
 
-	dataOut = sqrt(s1*s1 + s2*s2)/s0
+	dataOut = sqrt(s1*s1 + s2*s2)/parms(0)
 }
 
