14e167f74b6bb311ce4b417e039c4ffe72163b45
sberul <sberul@gmail.com>
2014-04-17 14:42:41 -0500

    Update AoP.scala
    Changed input to vector

--- polarization/src/main/scala/kernels/AoP.scala
+++ polarization/src/main/scala/kernels/AoP.scala
@@ -4,17 +4,16 @@ import scalapipe.dsl._
 
 class AoP(_name:String) extends Kernel(_name:String)
 {
-	val s0 = input(FLOAT32)
-	val s1 = input(FLOAT32)
-	val s2 = input(FLOAT32)
-
+	//s0 = parms(0), s1 = parms(1), s2 = parms(2)
+	val parms = input(Vector(FLOAT32, 4))
+	
 	val dataOut = output(FLOAT32)
 
 	val t1 = local(FLOAT32, 0)
 	val t2 = local(FLOAT32, 0)
 	val t3 = local(FLOAT32, 0)
 
-	t1 = s2/s1
+	t1 = parms(2) / parms(1)
 	t2 = t1*t1*t1
 	t3 = t2*t1*t1/5
 	t2 = t2/3
