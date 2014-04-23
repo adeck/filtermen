6c1941cb8adc916ca2dacc332ba2cf794e00c7aa
sberul <sberul@gmail.com>
2014-04-23 13:35:08 -0500

    added new blocks

---
+++ polarization/src/main/scala/kernels/Combine.scala
@@ -0,0 +1,17 @@
+package polarization.kernels
+import scalapipe._
+import scalapipe.dsl._
+
+class Combine(_name:String) extends Kernel(_name:String)
+{
+	//Combines intensity, degree, and angle into a vector, to be sent to GenHSV
+	val intensity = input(FLOAT32)
+	val degree = input(FLOAT32)
+	val angle = input(FLOAT32)
+	val dataOut = output(Vector(FLOAT32, 3)
+	val temp = local(Vector(FLOAT32,3))
+	temp(0)=intensity
+	temp(1) = degree
+	temp(2) = angle
+	dataOut = temp
+}
---
+++ polarization/src/main/scala/kernels/GenHSV.scala
@@ -0,0 +1,33 @@
+package polarization.kernels
+import scalapipe._
+import scalapipe.dsl._
+
+class GenHSV(_name:String) extends Kernel(_name:String)
+{
+	//parms(0) = intensity
+	//parms(1) = degree 
+	//parms(2) = angle
+	val parms = input(Vector(Float32, 3))
+	val dataOut = output(Vector(FLOAT32, 3))
+	val temp = local(Vector(FLOAT32,3))
+
+	if(parms(0) > 255){
+		temp(0) = 255
+	}
+	else {
+		temp(0) = parms(0)
+	}
+	if(parms(1) > 255){
+		temp(1) = 255
+	}
+	else {
+		temp(1) = parms(1)
+	}
+	if(parms(2) > 179)
+		temp(2) = 179
+	}
+	else {
+		temp(2) = parms(2)
+	}
+	dataOut = temp
+}
