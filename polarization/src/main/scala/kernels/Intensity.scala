1110cda8e3852733a17e3f541eb7c96745e74c81
sberul <sberul@gmail.com>
2014-04-24 13:38:11 -0500

    Update Intensity.scala

--- polarization/src/main/scala/kernels/Intensity.scala
+++ polarization/src/main/scala/kernels/Intensity.scala
@@ -2,6 +2,7 @@ package polarization.kernels
 import scalapipe._
 import scalapipe.dsl._
 
+//Gets its input from: Interpolation
 class Intensity(_name:String) extends Kernel(_name:String)
 {
 	val parms = input(Vector(FLOAT32, 4))
